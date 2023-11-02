e515f3743cc60d339a9ad73e33a95997b509d032
AswinSTS <aswin.s@deltasoft.in>
2023-11-02 13:57:51 +0530

    ds

--- rsvc_be/config/config.js
+++ rsvc_be/config/config.js
@@ -24,7 +24,7 @@ module.exports = {
       clientID: process.env.GOOGLE_CLIENTID || '986008663911-stfbfo4nqaea83dd5n7khgllpke05okf.apps.googleusercontent.com',
       clientSecret: process.env.GOOGLE_SECRET || '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',
       // callbackURL: "http://localhost:3000/api/auth/google/redirect",
-      callbackURL: 'https://654357d99f92cb2dc521c107--fanciful-pasca-c9a092.netlify.app/api/auth/google/redirect',
+      callbackURL: 'https://65435bc58fec0a298711d4af--deluxe-brioche-1a9ed4.netlify.app/api/auth/google/redirect',
       scope: ['profile', 'email'],
       profileFields: ['emails', 'name'],
     },
